87deaeeab07b13729fbb1f0131467708d2361dc8
Aniruddh Singh <aniruddhs@gmail.com>
2023-06-20 08:25:17 -0400

    changes to parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 
+
 #filter xml file by attribute key and value. Result is written to output file that only contains elements with the specified attribute key and value along with parents and child nodes
 def filter_xmlfile(input_file, output_file, attribute_name, attribute_value):
     tree = ET.parse(input_file)
@@ -13,29 +14,30 @@ def filter_xmlfile(input_file, output_file, attribute_name, attribute_value):
 
     filtered_root = ET.Element(root.tag)
 
-    # first add all the elements to the filtered root
+    #Add all the parent hierarchy of the filtered elements to the filtered root
     for element in filtered_elements:
-        if element not in filtered_root:
-            filtered_root.append(element)
-
-    #Now add all the parent hierarchy of the filtered elements to the filtered root
-
-    for element in filtered_elements:
+        # get the parent of the element
         parent = element.find("..")
         attrib_id="id"
-        print (f"element:{element.tag} element attrib:{element.attrib[attrib_id]} parent:{parent}")
+        parent_id="id"
+        
         while parent is not None:
             if parent not in filtered_root:
                 filtered_root.append(parent)
             parent = parent.find("..")
- 
-    #Also add all the children hierarchy of the filtered elements to the filtered root
-    '''
+            
+        if parent is not None:
+            print (f"element:{element.tag} element attrib:{element.attrib[attrib_id]} parent:{parent.tag} parent attrib:{parent.attrib[parent_id]}")
+        else:
+            print (f"element:{element.tag} element attrib:{element.attrib[attrib_id]} parent:{parent}")
+
+
+    # Now add all the elements to the filtered root
     for element in filtered_elements:
-        for child in element:
-            if child not in filtered_root:
-                filtered_root.append(child)
-    '''
+        if element not in filtered_root:
+            filtered_root.append(element)
+
+
     filtered_tree = ET.ElementTree(filtered_root)
 
     #Now write the filtered root to the output file
@@ -44,8 +46,8 @@ def filter_xmlfile(input_file, output_file, attribute_name, attribute_value):
 
 if __name__=='__main__':
 
-    input_file = 'test.xml'
-    output_file = 'test_out.xml'
+    input_file = 'simple.xml'
+    output_file = 'output.xml'
     attribute_name = 'state'
     attribute_value = 'ny'
 
