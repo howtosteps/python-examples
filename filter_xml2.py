880594362ba3e52abdedf3d9d80b1cb5126a0dc4
Aniruddh Singh <aniruddhs@gmail.com>
2023-07-16 20:29:55 -0400

    updates to sample

--- filter_xml2.py
+++ filter_xml2.py
@@ -2,39 +2,34 @@ import xml.etree.ElementTree as ET
 
 # Recursively traverse the original XML tree and add necessary elements to the filtered XML tree
 def add_elements(element, filtered_element, element_name, attribute_name, attribute_value):
-    bool_tag_found = False
+    #bool_tag_found = False
 
     for child in element:
         print (f"child:{child.tag} child attrib:{child.attrib} child text:{child.text}")
 
         # Recursively call this function for each child element
         if child.tag==element_name:
-            bool_tag_found = True
+            #bool_tag_found = True
             # Check if the element has the specified attribute name and value
             if attribute_name in child.attrib and child.attrib[attribute_name] == attribute_value:
                 print ("++Adding")
                 # Add the element to the filtered XML tree
                 filtered_element_copy = ET.Element(child.tag, child.attrib)
+                filtered_element_copy.text = child.text  # Include child text
                 filtered_element_copy.extend(child)
                 filtered_element.append(filtered_element_copy)
             else:
                 print ("--Skipping")
-                pass
         else:
             # Add the child element to the filtered XML tree
-            print ("##Adding")
             child_element = ET.Element(child.tag, child.attrib)
-            child_tag_found=add_elements(child, child_element, element_name, attribute_name, attribute_value)
-            
-            if child_tag_found:
-                if len(child_element) > 0:
-                    filtered_element.append(child_element)
-            else:
-                child_element.extend(child)
-                if len(child_element) > 0:
-                    filtered_element.append(child_element)
+            child_element.text = child.text  # Include child text
+            #child_element.extend(child)
+            filtered_element.append(child_element)
 
-    return bool_tag_found
+            #if len(child_element) > 0:
+            #    filtered_element.append(child_element)
+            add_elements(child, filtered_element, element_name, attribute_name, attribute_value)
 
 # Recursively print the XML tree
 def print_element(element):
@@ -51,7 +46,11 @@ def recreate_filtered_xml(input_file, element_name, attribute_name, attribute_va
     filtered_root = ET.Element(root.tag)  # Create a new root element for the filtered XML tree
 
     add_elements(root, filtered_root, element_name, attribute_name, attribute_value)
+    #print("print root:")
     #print_element(root)
+
+    #print("print filtered root:")
+    #print_element(filtered_root)
 
     filtered_tree = ET.ElementTree(filtered_root)
 
@@ -60,11 +59,11 @@ def recreate_filtered_xml(input_file, element_name, attribute_name, attribute_va
 
 
 if __name__=='__main__':
-    input_file = 'input2.xml'
+    input_file = 'input.xml'
     output_file = 'output.xml'
-    element_name = 'model'
-    attribute_name = 'id'
-    attribute_value = 'm1'
+    element_name = 'security'
+    attribute_name = 'primarid'
+    attribute_value = 'FN 1.0'
 
     print ("starting ...")
     recreate_filtered_xml(input_file, element_name, attribute_name, attribute_value, output_file)
---
+++ filter_xml3.py
@@ -0,0 +1,43 @@
+import xml.etree.ElementTree as ET
+import copy
+
+def delete_elements(input_file, element_name, attribute_name, attribute_value, output_file):
+    # Parse the original XML file
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+
+    # Create a deep copy of the root element
+    filtered_root = copy.deepcopy(root)
+
+    # Find and delete the elements with the specified element name, attribute name, and value
+    for element in filtered_root.iter(element_name):
+        print (f"element:{element.tag} element attrib:{element.attrib} element text:{element.text}")
+        if attribute_name in element.attrib and element.attrib[attribute_name] != attribute_value:
+            print ("++Deleting")
+            element.clear()
+            #filtered_root.remove(element)
+        else:
+            print ("--Skipping")
+
+    # Save the modified XML tree to the output file
+    filtered_tree = ET.ElementTree(filtered_root)
+    filtered_tree.write(output_file, encoding="utf-8", xml_declaration=True)
+
+
+# Recursively print the XML tree
+def print_element(element):
+    for child in element:
+        print (f"child:{child.tag} child attrib:{child.attrib} child text:{child.text}")
+        print_element(child)
+
+
+if __name__=='__main__':
+    input_file = 'input.xml'
+    output_file = 'output.xml'
+    element_name = 'security'
+    attribute_name = 'primarid'
+    attribute_value = 'FN 1.0'
+
+    print ("starting ...")
+    delete_elements(input_file, element_name, attribute_name, attribute_value, output_file)
+    print ("finished ....")
