155ee91907f6947c085e8db240beb1a73a66dfb4
Aniruddh Singh <aniruddhs@gmail.com>
2023-07-19 23:33:37 -0400

    added parent mapping

--- filter_xml3.py
+++ filter_xml3.py
@@ -9,13 +9,19 @@ def delete_elements(input_file, element_name, attribute_name, attribute_value, o
     # Create a deep copy of the root element
     filtered_root = copy.deepcopy(root)
 
+    # Build a mapping of element to parent element
+    parent_map = build_parent_mapping(filtered_root)
+
     # Find and delete the elements with the specified element name, attribute name, and value
     for element in filtered_root.iter(element_name):
         print (f"element:{element.tag} element attrib:{element.attrib} element text:{element.text}")
         if attribute_name in element.attrib and element.attrib[attribute_name] != attribute_value:
             print ("++Deleting")
-            element.clear()
-            #filtered_root.remove(element)
+            #element.clear()
+            #element.set('deleted', 'yes')
+            parent = parent_map[element]
+            if parent is not None:
+                parent.remove(element)
         else:
             print ("--Skipping")
 
@@ -23,6 +29,11 @@ def delete_elements(input_file, element_name, attribute_name, attribute_value, o
     filtered_tree = ET.ElementTree(filtered_root)
     filtered_tree.write(output_file, encoding="utf-8", xml_declaration=True)
 
+def build_parent_mapping(element, parent=None):
+    mapping = {element: parent}
+    for child in element:
+        mapping.update(build_parent_mapping(child, element))
+    return mapping
 
 # Recursively print the XML tree
 def print_element(element):
